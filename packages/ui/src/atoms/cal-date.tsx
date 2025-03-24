f111372c65b4095a5f3d87dfcc760e9d30f23992
byeong <qudcks4885@gmail.com>
2025-03-24 17:10:00 +0900

    fix: add `isNotThisMonth` props to display prev or next month's date

--- packages/ui/src/atoms/cal-date.tsx
+++ packages/ui/src/atoms/cal-date.tsx
@@ -1,12 +1,16 @@
 interface CalDateProps {
   day: number;
   isToday: boolean;
+  isNotThisMonth?: boolean;
 }
 
-export const CalDate = ({ day, isToday }: CalDateProps) => {
+export const CalDate = ({ day, isToday, isNotThisMonth }: CalDateProps) => {
   const dayTextColor = isToday
     ? "text-texticon-onstandard-invert text-caption-600"
+    : isNotThisMonth
+    ? "text-texticon-onstandard-lowemphasis text-caption-400"
     : "text-texticon-onstandard-highemphasis text-caption-400";
+
   const dayBgColor = isToday ? "bg-surface-brand-sub" : "bg-transparent";
   return (
     <p
